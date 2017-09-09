2483beb018041c230b584d4e616a1525d6005ce5
Tapas Datta <tapash.me@gmail.com>
2017-09-10 00:33:52 +0600

    added example one

--- observer/ex-one.php
+++ observer/ex-one.php
@@ -1,82 +1,83 @@
 <?php
-interface Observer {
-    public function addCurrency(Currency $currency);
+
+interface Subject {
+    public function attach(Observer $observer);
+    public function notify();
 }
- 
-class priceSimulator implements Observer {
-    private $currencies;
-     
-    public function __construct() {
-        $this->currencies = array();
+
+
+interface Observer {
+    public function handle();
+}
+
+
+class Login implements Subject {
+
+    private $observers = [];
+
+    public function attach(Observer $observer)
+    {
+        $this->observers[] = $observer;
+
+        return $this;
     }
-     
-    public function addCurrency(Currency $currency) {
-        array_push($this->currencies, $currency);
+
+    public function detach($index)
+    {
+        unset($this->observers[$index]);
     }
-     
-    public function updatePrice() {
-        foreach ($this->currencies as $currency) {
-            $currency->update();
+
+    public function notify()
+    {
+        foreach ($this->observers as $observer) {
+            $observer->handle();
         }
     }
+
+    public function fire()
+    {
+        $this->notify();
+    }
 }
- 
-interface Currency {
-    public function update();
-    public function getPrice();
+
+class LogHandler implements Observer {
+
+    public function handle()
+    {
+        var_dump('Log to something important!');
+    }
 }
- 
-class Pound implements Currency {
-    private $price;
-     
-    public function __construct($price) {
-        $this->price = $price;
-        echo "<p>Pound Original Price: {$price}</p>";
+
+class EmailNotifier implements Observer {
+
+    public function handle()
+    {
+        var_dump('Email to something important!');
     }
-     
-    public function update() {
-        $this->price = $this->getPrice();
-        echo "<p>Pound Updated Price : {$this->price}</p>";
-    }
-     
-    public function getPrice() {
-        return f_rand(0.65, 0.71);
-    }
-     
 }
- 
-class Yen implements Currency {
-    private $price;
- 
-    public function __construct($price) {
-        $this->price = $price;
-        echo "<p>Yen Original Price : {$price}</p>";
-    }
- 
-    public function update() {
-        $this->price = $this->getPrice();
-        echo "<p>Yen Updated Price : {$this->price}</p>";
-    }
-     
-    public function getPrice() {
-        return f_rand(120.52, 122.50);
-    }
-     
-}
- 
-function f_rand($min=0,$max=1,$mul=1000000){
-    if ($min>$max) return false;
-    return mt_rand($min*$mul,$max*$mul)/$mul;
-}
- 
-$priceSimulator = new priceSimulator();
- 
-$currency1 = new Pound(0.60);
-$currency2 = new Yen(122);
- 
-$priceSimulator->addCurrency($currency1);
-$priceSimulator->addCurrency($currency2);
- 
-echo "<hr />";
-$priceSimulator->updatePrice();
- 
+
+
+
+$login = new Login;
+
+$login->attach(new LogHandler)->attach(new EmailNotifier);
+
+$login->fire();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
